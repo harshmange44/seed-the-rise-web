a74e9a147253fe33db740eec17bbecc35486c6d8
Harsh Mange <harshmange44@gmail.com>
2021-10-25 21:28:16 +0530

    .

--- routes/instance.js
+++ routes/instance.js
@@ -121,7 +121,12 @@ router.get("/:id", async (req, res) => {
 router.get("/sensordata/:id", async (req, res) => {
   try {
     const sensor_data = await Instance.findOne({name: req.params.id}, {sensor_data: 1});
-    res.status(200).json(sensor_data);
+    res.status(200).json({
+      air: sensor_data.air,
+      soil: sensor_data.soil,
+      ldr: sensor_data.ldr,
+      temperature_humidity: sensor_data.temperature_humidity
+    });
   } catch (err) {
     res.status(500).json(err);
   }
