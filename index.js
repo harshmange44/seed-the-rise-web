a3de3da6214a3df7540758d2adf20d0af84691e6
Harsh Mange <harshmange44@gmail.com>
2021-10-29 13:48:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const mongoose = require("mongoose");
 const instanceRoute = require("./routes/instance");
 const bodyParser = require("body-parser");
 const path = require("path");
-// const { initializeApp } = require('firebase-admin/app');
-// import * as admin from "firebase-admin";
-// const firebase = require('firebase/app');
-// const initializeApp = require("firebase/app");
-// const getAnalytics = require("firebase/analytics");
 
 dotenv.config();
 app.use(express.json());
@@ -28,37 +23,7 @@ app.use(bodyParser.urlencoded(
     { extended:true }
 ))
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA8ZKf8npZI2y1EnHgMsbBv38Cow6y6amc",
-//   authDomain: "seed-the-rise.firebaseapp.com",
-//   projectId: "seed-the-rise",
-//   storageBucket: "seed-the-rise.appspot.com",
-//   messagingSenderId: "570317396666",
-//   appId: "1:570317396666:web:939cbe767f24a43ff1c083",
-//   measurementId: "G-D6J7KY78GD"
-// };
-
-// Initialize Firebase
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-// const analytics = firebase.getAnalytics(firebaseApp);
-
-// admin.initializeApp({
-//   credential: admin.credential.cert({
-//       projectId: process.env.PROJECT_ID,
-//       privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
-//       clientEmail: process.env.CLIENT_EMAIL,
-//   }),
-//   databaseURL: process.env.DATABASE_URL
-// });
-
 app.use("/api/instances", instanceRoute);
-
-// if(process.env.NODE_ENV == "production"){
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running...")
