cf008fbcc2969f44257a8fa0c7d6831e062a9988
Harsh Mange <harshmange44@gmail.com>
2021-11-10 14:57:10 +0530

    Update instance.js
    .

--- routes/instance.js
+++ routes/instance.js
@@ -65,7 +65,7 @@ router.put("/:id?", async (req, res) => {
           },
           { new: true }
         );
-        res.status(200).json(updatedInst);
+        res.status(200).json(req.body);
       } catch (err) {
         res.status(500).json(err);
       }
