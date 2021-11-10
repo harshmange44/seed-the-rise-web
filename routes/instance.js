af1d3d4b71fcdfc9833cf6b9178694e63e4cbb07
Harsh Mange <harshmange44@gmail.com>
2021-11-10 12:50:10 +0530

    Update instance.js
    .

--- routes/instance.js
+++ routes/instance.js
@@ -35,11 +35,11 @@ router.post("/:id", async (req, res) => {
 //UPDATE INSTANCE
 router.put("/:id?", async (req, res) => {
       try {
-        const air = req.query.air;
-        const soil = req.query.soil;
-        const ldr = req.query.ldr;
-        const temperature = req.query.temperature;
-        const humidity = req.query.humidity;
+        const air = req.body.air;
+        const soil = req.body.soil;
+        const ldr = req.body.ldr;
+        const temperature = req.body.temperature;
+        const humidity = req.body.humidity;
 
         const updatedInst = await Instance.findOneAndUpdate(
           {name: req.params.id},
